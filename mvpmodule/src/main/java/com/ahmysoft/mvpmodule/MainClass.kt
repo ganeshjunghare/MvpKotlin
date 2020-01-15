85c88314025e1caf88009518aca25f74ab2e96eb
Yogesh Pimpalkar <yogesh.pimpalkar@tratagroup.com>
2020-01-15 11:05:55 +0530

    CR:Yogesh Pimpalkar: create lib

---
+++ mvpmodule/src/main/java/com/ahmysoft/mvpmodule/MainClass.kt
@@ -0,0 +1,26 @@
+package com.ahmysoft.mvpmodule
+
+import com.vansh.mvpkotlin.MainActivityPresentImpl
+import com.vansh.mvpkotlin.MainActivityPresenter
+import com.vansh.mvpkotlin.MainActivityView
+
+class MainClass: MainActivityView {
+    var presenter : MainActivityPresenter? = MainActivityPresentImpl(this)
+
+    private fun callApi(userName : String){
+        presenter?.submitUserInfo(userName)
+    }
+
+    override fun showLoading() {
+
+    }
+
+    override fun hideLoading() {
+
+
+    }
+
+    override fun displayUserInfo(userName: String) {
+
+    }
+}
